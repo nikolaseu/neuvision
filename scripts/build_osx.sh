d5d33ed50f68942712e76c3d173b13da979ed9aa
Nicolas Ulrich <nikolaseu@gmail.com>
2020-05-31 16:48:50 +0200

    Update osx/xcode version and fix build on travis

--- scripts/build_osx.sh
+++ scripts/build_osx.sh
@@ -2,4 +2,19 @@
 export Qt5_DIR=$(brew --prefix qt)
 export OpenCV_DIR=$(brew --prefix opencv)
 
-. $(dirname "$0")/build_linux.sh
+BUILD_TYPE=Release
+STARTING_DIR=$(pwd)
+BUILD_DIR=$STARTING_DIR/build/$BUILD_TYPE
+INSTALL_DIR=$STARTING_DIR/build/install/$BUILD_TYPE
+
+mkdir "$BUILD_DIR"
+cd "$BUILD_DIR"
+
+cmake $STARTING_DIR \
+	-DCMAKE_OSX_SYSROOT="/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk" \
+	-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+	-DCMAKE_INSTALL_PREFIX=$INSTALL_DIR
+
+cmake --build . --config $BUILD_TYPE
+
+cmake --build . --target install --config $BUILD_TYPE
